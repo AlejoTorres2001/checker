bfbb61f782525711afda3d6e7cc913cd840d8363
AlejoTorres2001 <torres.alejo2001@gmail.com>
2025-03-13 21:49:15 -0300

    add: consignas file

---
+++ utils/consignas.py
@@ -0,0 +1,13 @@
+EJOB_1_B ="""Dada una matriz (de string) que contiene palabras. Realizar un programa que realice un menú queinvoque a procedimientos o funciones para:
+Cargar la matriz
+Mostrar las palabras palíndromas indicando la fila y columna donde se encuentren dichaspalabras.
+Mostrar las palabras con más caracteres
+Mostrar las palabras que se repiten."""
+
+EJOB_1_A = """Dado una matriz de MxN, donde las columnas las primeras N-1 columnas contienen palabras y la última
+columna N contienen espacios en blanco. Usar procedimientos o funciones para:
+ cargar la matriz,
+ modificar la matriz reemplazando la última columna por el total de vocales o el total de
+consonantes de los elementos de las columnas 1..N-1. El usuario determina si quiere modificar
+por total de vocales o por total de consonantes
+ mostrar la matriz"""
